2f2771b29334c4443c9348533aa8d4fa2fcae160
Ben Teichman <ben.l.teichman@gmail.com>
2016-08-15 18:58:21 -0400

    add filter function and options

--- index.d.ts
+++ index.d.ts
@@ -1,6 +1,11 @@
-declare namespace FilterObject {
-  function filterObject(obj: any, glob: string): any;
-  function filterObject(obj: any, globs: string[]): any;
+declare function filterObject(obj: any, globs: string | string[], opts?: FilterOptions): any;
+declare function filterObject(obj: any, filter: Function, opts?: FilterOptions): any;
+
+declare interface FilterOptions {
+  keys?: string[];
+  sort?: (key1: string, key2: string) => number;
+  sortOrder?: 'asc' | 'desc' | 'ASC' | 'DESC' | string;
+  sortBy?: (obj: any) => string[];
 }
 
-export = FilterObject.filterObject;
+export = filterObject;
